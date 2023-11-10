0430135836d9d7e593808279b0c9a47b679e21f1
jordimall <jordi.mallafre@hotmail.com>
2023-11-10 14:30:49 +0100

    First commit

---
+++ src/main/java/com/example/demo/Ta26SpringMysqlEjercicio03Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ta26SpringMysqlEjercicio03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ta26SpringMysqlEjercicio03Application.class, args);
+	}
+
+}
